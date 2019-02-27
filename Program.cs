95debd04a297809399695046d35d2fcbe19936d7
WA <45290206+wgreen80021@users.noreply.github.com>
2019-02-27 04:30:40 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
  
-
-namespace ExcVarAndDataType
+namespace DrillPayRate
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The Tech Academy");
-            Console.WriteLine("Student Daily Report");
-            Console.WriteLine("What Course are you on ? ");
-            string courseName = Console.ReadLine();
-            Console.WriteLine("What page number ? ");
-            string intPageNum = Console.ReadLine();
-            Console.WriteLine("Do you need help with anything ? Please answer true or false");
-            string boolHelp = Console.ReadLine();
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            string strExp = Console.ReadLine();
-            Console.WriteLine("How many hours did you study today?");
-            string intHour = Console.ReadLine();
-            
-            //Console.WriteLine("You have answered the following questions :  ") ;
 
-            //Console.WriteLine("What Course are you on >  " + courseName );
-            //Console.WriteLine("What page number >  " + intPageNum);
-            //Console.WriteLine("Do you need help with anything ? Please answer true or false >  " + boolHelp);
-            //Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics > " + strExp);
-            //Console.WriteLine("How many hours did you study today > " + intHour);
-           
-            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day! ");
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1:");
+            Console.WriteLine("Please enter your hourly rate: ");
+            string Per1HR = Console.ReadLine();
+            Console.WriteLine("Please enter Hours Worked : ");
+            string Per1HW = Console.ReadLine();
+
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("Please enter hourly rate : ");
+            string Per2HR = Console.ReadLine();
+            Console.WriteLine("Please enter Hours Worked per week : ");
+            string Per2HW = Console.ReadLine();
+
+            decimal P1Ann = ((Convert.ToDecimal(Per1HR) * Convert.ToDecimal(Per1HW)) * 52);
+            decimal P2Ann = ((Convert.ToDecimal(Per2HR) * Convert.ToDecimal(Per2HW)) * 52);
+
+            Console.WriteLine("Annual Salary for Person 1 : ");
+            Console.WriteLine("$" + P1Ann);
+            //Console.ReadLine();
+
+            Console.WriteLine("Annual Salary for Person 2 : ");
+            Console.WriteLine("$" + P2Ann);
+
+            Console.WriteLine("Does Person 1 make more than Person 2 ?  ");
+            bool TrueFalse = Convert.ToDecimal(P1Ann) > Convert.ToDecimal(P2Ann);
+            Console.WriteLine(TrueFalse);
             Console.ReadLine();
-
-
-
-
-
         }
     }
 }
